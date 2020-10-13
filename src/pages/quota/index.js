06ee7ab8349fab2790c11c89be1a61ee8390289c
DoDuongDuy <duyhoten080502@gmail.com>
2020-10-13 23:08:19 +0700

    update

--- src/pages/quota/index.js
+++ src/pages/quota/index.js
@@ -1,39 +1,81 @@
 import Vue from 'vue';
 import template from './template.html';
+import DropdownList from '../../components/dropdown-list';
 import config from '../../config';
 import axios from 'axios';
 console.log(config);
+
 let component = {
-  data: function() {
+  data: function (){
     return {
-      contents:[],
-      tabIdx : 0,
+      contents: [],
+      tabIdx: 0,
+      quotaData: {},
+      currentQuotaId: null,
+      editQuotaId:null,
+      editQuotaYear:null,
+      editQuotaIndex:null,
     };
   },
-  async created(){
-    try{
-      const res = await axios.get(config.QUOTAS_URL);
-      this.contents = res.data;
-    }catch(err){
-      console.log(err);
-    }
+  created: function() {
+    this.loadData();
   },
   methods: {
-    deleteContent: async function(id){
-      try{
-        axios.delete(config.QUOTAS_URL)
-      }catch(err){
-        console.log(err);
-      }
+    loadData: function() {
+      axios.get(config.QUOTAS_URL).then(res => {
+        this.contents = res.data.sort((item1, item2) => (item2.idQuota - item1.idQuota));
+      }).catch(err => console.error(err));
     },
-    // addContent: async function() {
-    //   const res = await axios.post(config.QUOTAS_URL, { name: this.contents })
-    //   this.contents = [...this.contents, res.data]
-    //   this.contents = ''
-    // }
+
+    createQuota: function(quotaData) {
+      console.log('click');
+      axios({
+        method:"post",
+        url:config.QUOTAS_URL,
+        headers:{
+          'Content-Type':'application/json'
+        },
+        data: quotaData
+      }).then((res)=>{
+        console.log(res.data);
+        this.tabIdx = 0;
+        this.loadData();
+      }).catch((err)=>{console.log(err)});
+    },
+    editQuota: function(idquota){
+      console.log("edit")
+      axios({
+        method:"put",
+        url:config.QUOTAS_URL + idquota,
+      }).then(res=>{
+        console.log(res.data);
+      }).catch()
+    },
+
+    deleteQuota: function(idQuota) {
+      console.log('delete');
+      axios({
+        method: 'delete',
+        url: config.QUOTAS_URL + idQuota
+      }).then(res => {
+        console.log(res.data);
+        this.loadData();
+        this.tabIdx = 0;
+      }).catch(
+        e => console.error(e)
+      );
+    },
+    
+    selectChanged: function(selectedItem, selectedIdx) {
+      console.log(selectedItem);
+      this.quotaData.quotaIndex = selectedIdx
+    }
+
   },
+  
   template,
+  components: {
+    DropdownList
+  }
 };
-
-
-export default { path:"/quota", component: component }
+export default { path: "/quota", component: component }
